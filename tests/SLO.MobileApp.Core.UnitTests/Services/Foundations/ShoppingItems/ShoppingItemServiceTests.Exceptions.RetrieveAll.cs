a357b5a409043780e72ed640b0095b989e5be7a1
Mohammed Elamin <mohammed.qahwagi@outlook.com>
2026-03-13 19:35:07 +0100

    ShouldThrowCriticalDependencyExceptionOnRetrieveAllIfSqlErrorOccursAndLogItAsync -> FAIL

---
+++ tests/SLO.MobileApp.Core.UnitTests/Services/Foundations/ShoppingItems/ShoppingItemServiceTests.Exceptions.RetrieveAll.cs
@@ -0,0 +1,58 @@
+﻿using Moq;
+using SLO.MobileApp.Core.Models.Foundations.ShoppingItems;
+using SLO.MobileApp.Core.Models.Foundations.ShoppingItems.Exceptions;
+using SLO.MobileApp.Core.UnitTests.Helpers;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SLO.MobileApp.Core.UnitTests.Services.Foundations.ShoppingItems;
+
+public partial class ShoppingItemServiceTests
+{
+    [Fact]
+    public async Task ShouldThrowCriticalDependencyExceptionOnRetrieveAllIfSqlErrorOccursAndLogItAsync()
+    {
+        // given
+        var sqlException = Randomizers.GetSqlException();
+
+        var failedShoppingItemStorageException =
+            new FailedShoppingItemStorageException(
+                exceptionMessage: "Failed shopping item storage error occurred, " +
+                "please contact support.",
+                innerException: sqlException);
+
+        var expectedShoppingItemDependencyException =
+            new ShoppingItemDependencyException(
+                exceptionMessage: "Shopping item dependency error occurred, " +
+                "please contact support.",
+                innerException: failedShoppingItemStorageException);
+
+        _storageBrokerMock.Setup(broker =>
+            broker.SelectAllShoppingItemsAsync(
+                It.IsAny<CancellationToken>()))
+            .ThrowsAsync(sqlException);
+
+        // when
+        ValueTask<IQueryable<ShoppingItem>> retrieveAllShoppingItemsTask =
+            _shoppingItemService.RetrieveAllShoppingItemsAsync(
+                It.IsAny<CancellationToken>());
+
+        await Assert.ThrowsAsync<ShoppingItemDependencyException>(
+            retrieveAllShoppingItemsTask.AsTask);
+
+        // then
+        _storageBrokerMock.Verify(broker =>
+            broker.SelectAllShoppingItemsAsync(
+                It.IsAny<CancellationToken>()),
+            Times.Once());
+
+        _loggingBrokerMock.Verify(broker =>
+            broker.LogCriticalAsync(
+                It.Is(Randomizers.SameExceptionAs(
+                    expectedShoppingItemDependencyException))),
+            Times.Once());
+
+        VerifyNoOtherDependencyCalls();
+    }
+}
