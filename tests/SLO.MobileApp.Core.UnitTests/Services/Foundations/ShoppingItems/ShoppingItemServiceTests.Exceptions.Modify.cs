e5ea1a2a2f5378923cf78db21da55578d474deb7
Mohammed Elamin <mohammed.qahwagi@outlook.com>
2026-03-13 17:58:39 +0100

    ShouldThrowDependencyExceptionOnModifyIfDbUpdateErrorOccursAndLogItAsync -> PASS

--- tests/SLO.MobileApp.Core.UnitTests/Services/Foundations/ShoppingItems/ShoppingItemServiceTests.Exceptions.Modify.cs
+++ tests/SLO.MobileApp.Core.UnitTests/Services/Foundations/ShoppingItems/ShoppingItemServiceTests.Exceptions.Modify.cs
@@ -80,4 +80,74 @@ public partial class ShoppingItemServiceTests
 
         VerifyNoOtherDependencyCalls();
     }
+
+    [Fact]
+    public async Task ShouldThrowDependencyExceptionOnModifyIfDbUpdateErrorOccursAndLogItAsync()
+    {
+        // given
+        DateTimeOffset currentDateTime = Randomizers.GetRandomDateTime();
+
+        ShoppingItem randomShoppingItem =
+            CreateRandomShoppingItem(currentDateTime);
+
+        randomShoppingItem.UpdatedAt =
+            randomShoppingItem.UpdatedAt.AddMinutes(1);
+
+        var exceptionMessage = Randomizers.GetRandomString();
+
+        var dbUpdateException =
+            new DbUpdateException(exceptionMessage);
+
+        var failedShoppingItemStorageException =
+            new FailedShoppingItemStorageException(
+                exceptionMessage: "Failed shopping item storage error occurred, " +
+                "please contact support.",
+                innerException: dbUpdateException);
+
+        var expectedShoppingItemDependencyException =
+            new ShoppingItemDependencyException(
+                exceptionMessage: "Shopping item dependency error occurred, " +
+                "please contact support.",
+                innerException: failedShoppingItemStorageException);
+
+        _dateTimeBrokerMock.Setup(broker =>
+            broker.GetCurrentDateTimeAsync(
+                It.IsAny<CancellationToken>()))
+            .ThrowsAsync(dbUpdateException);
+
+        // when
+        ValueTask<ShoppingItem> modifyShoppingItemTask =
+            _shoppingItemService.ModifyShoppingItemAsync(
+                randomShoppingItem,
+                It.IsAny<CancellationToken>());
+
+        await Assert.ThrowsAsync<ShoppingItemDependencyException>(
+            modifyShoppingItemTask.AsTask);
+
+        // then
+        _dateTimeBrokerMock.Verify(broker =>
+            broker.GetCurrentDateTimeAsync(
+                It.IsAny<CancellationToken>()),
+            Times.Once());
+
+        _storageBrokerMock.Verify(broker =>
+            broker.SelectShoppingItemByIdAsync(
+                It.IsAny<Guid>(),
+                It.IsAny<CancellationToken>()),
+            Times.Never());
+
+        _storageBrokerMock.Verify(broker =>
+            broker.UpdateShoppingItemAsync(
+                It.IsAny<ShoppingItem>(),
+                It.IsAny<CancellationToken>()),
+            Times.Never());
+
+        _loggingBrokerMock.Verify(broker =>
+            broker.LogErrorAsync(
+                It.Is(Randomizers.SameExceptionAs(
+                    expectedShoppingItemDependencyException))),
+            Times.Once());
+
+        VerifyNoOtherDependencyCalls();
+    }
 }
