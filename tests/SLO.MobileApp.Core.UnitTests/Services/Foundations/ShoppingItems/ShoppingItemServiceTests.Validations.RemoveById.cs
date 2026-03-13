9e1f92bb29709eb9f7c65871eae0bf6ec6d93512
Mohammed Elamin <mohammed.qahwagi@outlook.com>
2026-03-13 21:38:21 +0100

    ShouldThrowValidationExceptionOnRemoveByIdIfShoppingItemIdIsNotFoundAndLogItAsync -> FAIL

--- tests/SLO.MobileApp.Core.UnitTests/Services/Foundations/ShoppingItems/ShoppingItemServiceTests.Validations.RemoveById.cs
+++ tests/SLO.MobileApp.Core.UnitTests/Services/Foundations/ShoppingItems/ShoppingItemServiceTests.Validations.RemoveById.cs
@@ -62,4 +62,61 @@ public partial class ShoppingItemServiceTests
 
         VerifyNoOtherDependencyCalls();
     }
+
+    [Fact]
+    public async Task ShouldThrowValidationExceptionOnRemoveByIdIfShoppingItemIdIsNotFoundAndLogItAsync()
+    {
+        // given
+        Guid notFoundShoppingItemId = Guid.NewGuid();
+        Guid shoppingItemId = notFoundShoppingItemId;
+        ShoppingItem nullShoppingItem = null;
+        ShoppingItem storageShoppingItem = nullShoppingItem;
+
+        var notFoundShoppingItemException =
+            new NotFoundShoppingItemException(
+                exceptionMessage: $"A shopping item with Id: {shoppingItemId}, " +
+                $"could not be found.");
+
+        var expectedShoppingItemValidationException =
+            new ShoppingItemValidationException(
+                exceptionMessage: "Shopping item validation error occurred, " +
+                "fix the errors and try again please!",
+                innerException: notFoundShoppingItemException);
+
+        _storageBrokerMock.Setup(broker =>
+            broker.SelectShoppingItemByIdAsync(
+                shoppingItemId,
+                It.IsAny<CancellationToken>()))
+            .ReturnsAsync(storageShoppingItem);
+
+        // when
+        ValueTask<ShoppingItem> removeShoppingItemByIdTask =
+            _shoppingItemService.RemoveShoppingItemByIdAsync(
+                shoppingItemId,
+                It.IsAny<CancellationToken>());
+
+        await Assert.ThrowsAsync<ShoppingItemValidationException>(
+            removeShoppingItemByIdTask.AsTask);
+
+        // then
+        _storageBrokerMock.Verify(broker =>
+            broker.SelectShoppingItemByIdAsync(
+                shoppingItemId,
+                It.IsAny<CancellationToken>()),
+            Times.Once());
+
+        _storageBrokerMock.Verify(broker =>
+            broker.DeleteShoppingItemAsync(
+                It.IsAny<ShoppingItem>(),
+                It.IsAny<CancellationToken>()),
+            Times.Never());
+
+        _loggingBrokerMock.Verify(broker =>
+            broker.LogErrorAsync(
+                It.Is(Randomizers.SameExceptionAs(
+                    expectedShoppingItemValidationException))),
+            Times.Once());
+
+        VerifyNoOtherDependencyCalls();
+    }
 }
